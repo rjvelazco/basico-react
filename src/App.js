6ce278295d57103c431c674573d1b13fb378b309
rjvelazco <rjvelazco21@gmail.com>
2021-04-02 23:02:04 -0400

    Conceptos de ReactJS - Components & Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+
+  // Obtener la fecha
+  const fecha = new Date().getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header 
+        titulo='Tienda Virtual'
+        numero={20}
+      />
+      <Footer 
+        fecha={fecha}
+      />
+    </Fragment>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header({titulo}) {
+    return(
+        <h1 className="encabezado" id="cabezado">{titulo}</h1>
+    )
+}
+
+export default Header;
+
